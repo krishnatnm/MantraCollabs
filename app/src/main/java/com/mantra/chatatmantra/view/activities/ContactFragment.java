3b9e2f8f70e240d4ea8371745fc8885e663e715c
Rajat Gupta <rajatgupta0889@gmail.com>
2016-05-28 14:08:28 +0530

    tabs

---
+++ app/src/main/java/com/mantra/chatatmantra/view/activities/ContactFragment.java
@@ -0,0 +1,14 @@
+package com.mantra.chatatmantra.view.activities;
+
+import android.support.v4.app.Fragment;
+
+import com.mantra.chatatmantra.R;
+
+import org.androidannotations.annotations.EFragment;
+
+/**
+ * Created by rajat on 28/05/16.
+ */
+@EFragment(R.layout.contact_fragment_layout)
+public class ContactFragment extends Fragment {
+}
--- app/src/main/java/com/mantra/chatatmantra/view/activities/LoginActivity.java
+++ app/src/main/java/com/mantra/chatatmantra/view/activities/LoginActivity.java
@@ -1,13 +1,13 @@
 package com.mantra.chatatmantra.view.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.mantra.chatatmantra.R;
 
@@ -24,8 +24,7 @@ public class LoginActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                TabActivity_.intent(getApplicationContext()).flags(Intent.FLAG_ACTIVITY_NEW_TASK).start();
             }
         });
     }
